5fe5418a8cb7b815630c6e6799471205715e37f3
Hochul Song <hochuldev@naver.com>
2022-05-22 11:05:57 +0900

    implemented a program that can calculate runtime of a method

---
+++ calculateRuntime.js
@@ -0,0 +1,24 @@
+//shift와 unshift를 테스트하는거다.
+//createDataStructureOf
+
+function calculateRuntime() {
+    
+    for( let i = 0; i < 1000; i++ ){
+        let queue = createQueueOfSizeN( i );
+        let initialTime = new Date();
+        queue.unshift(35);
+        let finalTime = new Date();
+        let runtime = finalTime - initialTime;
+        console.log( `runtime when the size of the queue is ${i}: ${runtime}` );
+    }
+}
+
+function createQueueOfSizeN( N ) {
+    let queue = [];
+    for( let i = 0; i < N; i++ ){
+        queue.push(i);
+    }
+    return queue;
+}
+
+calculateRuntime();
