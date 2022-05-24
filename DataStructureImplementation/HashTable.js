85bb019f0050503a0880cf4859dade5e54c22c06
Hochul Song <hochuldev@naver.com>
2022-05-24 11:33:34 +0900

    implemented hash table that does not address collisions.

--- DataStructureImplementation/HashTable.js
+++ DataStructureImplementation/HashTable.js
@@ -1,3 +1,5 @@
+
+
 function Element(key, value) {
     this.key = key;
     this.value = value;
@@ -12,43 +14,42 @@ function HashTable( size ) {
 
 HashTable.prototype.add = function( key, value ) {
     const isKeyValueValid = typeof key == "string" && typeof value == "number";
-    if( !isKeyValueValid ) throw Error("The key must be a string and the value must be a number.");
+    if( !isKeyValueValid ) throw Error("The key must be a string and the value must be a number");
 
     const hashCode = this.hash( key );
     this.array[hashCode] = new Element(key, value);
 }
 
 HashTable.prototype.delete = function( key ) {
-    const isKeyValid = typeof key == "string";
-    if( !isKeyValid ) throw Error("The key must be a string.");
+    this.checkKeyValidity(key);
 
     const hashCode = this.hash( key );
     this.array.splice( hashCode, 1 );
 }
 
 HashTable.prototype.get = function( key ) {
-    const isKeyValid = typeof key == "string";
-    if( !isKeyValid ) throw Error("The key must be a string.");
+    this.checkKeyValidity(key);
 
     const hashCode = this.hash( key );
     return this.array[ hashCode ];
 }
 
 HashTable.prototype.hash = function( key ) {
+    this.checkKeyValidity(key);
+
     let hash = 5381;
     for( let i = 0; i < key.length; i++ ) {
         hash = hash * 33 + key.charCodeAt(0);
     }
     return hash % this.size;
+    // return hash;
 }
 
 HashTable.prototype.print = function() {
     console.log(this.array);
 }
 
-myHash = new HashTable(100);
-
-myHash.add("myFirstElement", 256);
-myHash.add("mySecondElement", 2123);
-
-myHash.print();
+HashTable.prototype.checkKeyValidity = function(key) {
+    const isKeyValid = typeof key == "string";
+    if( !isKeyValid ) throw Error("The key must be a string.");
+}
