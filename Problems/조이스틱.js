e2ba101a337833c02e85ff6f3c237e18a7eff2e9
Hochul Song <hochuldev@naver.com>
2022-06-12 17:26:46 +0900

    조이스틱 문제의 풀이를 더 구현함. undefined를 return하는 원인을 찾아야 함.

--- Problems/조이스틱.js
+++ Problems/조이스틱.js
@@ -1,23 +1,27 @@
 
-function solutionImplmentation(name, currentIndex ,possibleIndices) {
-  let counter = 0;
+function solutionImplementation(name, currentIndex ,possibleIndices) {
   let leftCounter, rightCounter;
   if( possibleIndices.length > 0 ) { // recursive case
-    counter += matchCharacter(name, currentIndex);
-    currentIndex = calcNextIndex(name, currentIndex, possibleIndices, "LEFT");
-    leftCounter = solutionImplmentation(name, currentIndex,  )
+    let currentCounter = matchCharacter(name, currentIndex);
+    let nextIndex = calcNextIndex(name, currentIndex, possibleIndices, "LEFT");
+    if( possibleIndices.indexOf(currentIndex) === -1) throw Error("currentIndex must be in possibleIndices");
+    let nextPossibleIndices = possibleIndices.filter( (value) => value !== currentIndex );
+    leftCounter = solutionImplementation(name, nextIndex, nextPossibleIndices );
+    nextIndex = calcNextIndex(name, currentIndex, possibleIndices, "RIGHT");
+    rightCounter = solutionImplementation(name, nextIndex, nextPossibleIndices);
+    return Math.min( leftCounter, rightCounter ) + currentCounter;
+  }
+  //아래는 base case
+  return 0;
   }
 
-}
-
-function updatePossibleIndices(name, currentIndex, possibleIndices) {
-  
-}
-
 function solution(name){
-  let possibleIndices = Array.from(name);
+  let possibleIndices = [];
+  for( let i = 0; i < name.length; i++ ){
+    possibleIndices.push(i);
+  }
   possibleIndices = createPossibleIndices(name, possibleIndices);
-  solutionImplmentation(name, 0, possibleIndices);
+  solutionImplementation(name, 0, possibleIndices);
 }
 
 function calcNextIndex(name, currentIndex, possibleIndices, direction){
@@ -48,83 +52,16 @@ function calcNextIndex(name, currentIndex, possibleIndices, direction){
     min[1] = min[1] < distance ? min[1] : distance;
     min[0] = min[1] < distance ? min[0] : key;
   });
-  return min;
-
+  return min[0];
 }
-
-// function solution(name) {
-//   let hasFinished = false;
-//   let currentIndex = 0;
-//   let counter = 0;
-//   let possibleIndices = [];
-//   for (let i = 1; i < name.length; i++) {
-//     possibleIndices.push(i);
-//   }
-//   while (!hasFinished) {
-//     counter += matchCharacter(name, currentIndex);
-//     possibleIndices = updatePossibleIndices(name, possibleIndices);
-//     if (possibleIndices.length !== 0) {
-//       let minAndIndices = moveToNextChar(name, currentIndex, possibleIndices);
-//       let min = minAndIndices.min;
-//       possibleIndices = minAndIndices.possibleIndices;
-//       currentIndex = minAndIndices.currentIndex;
-//       counter += min;
-//     } else hasFinished = true;
-//   }
-//   return counter;
-// }
 
 function createPossibleIndices(name, possibleIndices) {
   let tempIndices = [];
   possibleIndices.forEach((targetIndex) => {
-    if (name[targetIndex] !== "A" && targetIndex !== 0)
+    if (name[targetIndex] !== "A")
       tempIndices.push(targetIndex);
   });
   return tempIndices;
-}
-
-function moveToNextChar(name, currentIndex, possibleIndices) {
-  const distances = new Map();
-  possibleIndices.forEach((targetIndex) => {
-    let distance = 0;
-    let tempIndex = currentIndex;
-
-    while (tempIndex !== targetIndex) {
-      //오른쪽으로 움직이며 최소 거리를 계산하는 코드.
-      distance++;
-      tempIndex++;
-      tempIndex = tempIndex % name.length;
-    }
-    distances.set(targetIndex, distance); // 오른쪽으로 쭉 움직였을 때, targetIndex에 도달하기 까지의 시간.
-    distance = 0;
-    tempIndex = currentIndex;
-    while (tempIndex !== targetIndex) {
-      //왼쪽으로 움직이며 최소 거리를 계산하는 코드.
-      distance++;
-      tempIndex--;
-      if (tempIndex < 0) tempIndex = name.length + tempIndex;
-    }
-
-    if (distances.get(targetIndex) > distance) {
-      distances.set(targetIndex, distance);
-    }
-  });
-  distances.delete(currentIndex);
-
-  let min = ["", Number.POSITIVE_INFINITY];
-  distances.forEach((distance, key) => {
-    min[1] = min[1] < distance ? min[1] : distance;
-    min[0] = min[1] < distance ? min[0] : key;
-  });
-  if (min[0] === "" || min[1] === Number.POSITIVE_INFINITY) {
-    min[1] = 0;
-  }
-  possibleIndices.splice(possibleIndices.indexOf(min[0]), 1);
-  return {
-    min: min[1],
-    possibleIndices: possibleIndices,
-    currentIndex: min[0],
-  };
 }
 
 function matchCharacter(name, i) {
@@ -153,8 +90,7 @@ function matchCharacter(name, i) {
   }
   return counter;
 }
-
 // matchCharacter("JEROEN", 1);
 
-// console.log(solution("BG"));
-console.log(solution("BDFADHA"));
+console.log(solution("BG"));
+// console.log(solution("BDFADHA"));
