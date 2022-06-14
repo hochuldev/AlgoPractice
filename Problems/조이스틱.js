885a586712e54359bd54d6ba65f30f87e4e16061
Hochul Song <hochuldev@naver.com>
2022-06-14 19:28:51 +0900

    base case에서 currentCounter가 정의되지 않았다는 오류가 남.

--- Problems/조이스틱.js
+++ Problems/조이스틱.js
@@ -1,42 +1,69 @@
-
-function solutionImplementation(name, currentIndex ,possibleIndices) {
+function solutionImplementation(name, currentIndex, possibleIndices) {
   let leftCounter, rightCounter;
-  if( possibleIndices.length > 0 ) { // recursive case
+  if (possibleIndices.length > 0) {
+    // recursive case
     let currentCounter = matchCharacter(name, currentIndex);
-    let nextIndex = calcNextIndex(name, currentIndex, possibleIndices, "LEFT");
-    if( possibleIndices.indexOf(currentIndex) === -1) throw Error("currentIndex must be in possibleIndices");
-    let nextPossibleIndices = possibleIndices.filter( (value) => value !== currentIndex );
-    leftCounter = solutionImplementation(name, nextIndex, nextPossibleIndices );
-    nextIndex = calcNextIndex(name, currentIndex, possibleIndices, "RIGHT");
-    rightCounter = solutionImplementation(name, nextIndex, nextPossibleIndices);
-    return Math.min( leftCounter, rightCounter ) + currentCounter;
+    let nextIndexAndDistance = calcNextIndex(
+      name,
+      currentIndex,
+      possibleIndices,
+      "LEFT"
+    );
+    let nextIndex = nextIndexAndDistance[0];
+    let moveCounter = Math.abs(
+      typeof nextIndex === "number" ? nextIndexAndDistance[1] : 0
+    );
+    let nextPossibleIndices = possibleIndices.filter(
+      (value) => value !== currentIndex
+    );
+    leftCounter =
+      solutionImplementation(name, nextIndex, nextPossibleIndices) +
+      moveCounter;
+    nextIndexAndDistance = calcNextIndex(
+      name,
+      currentIndex,
+      possibleIndices,
+      "RIGHT"
+    );
+    nextIndex = nextIndexAndDistance[0];
+    moveCounter = Math.abs(
+      typeof nextIndex === "number" ? nextIndexAndDistance[1] : 0
+    );
+    rightCounter =
+      solutionImplementation(name, nextIndex, nextPossibleIndices) +
+      moveCounter;
+    console.log(
+      `possibleIndices: ${possibleIndices}, nextPossibleIndices: ${nextPossibleIndices}, leftCounter: ${leftCounter}, rightCounter: ${rightCounter}, moveCounter: ${moveCounter}, currentCounter: ${currentCounter}`
+    );
+
+    
   }
   //아래는 base case
-  return 0;
-  }
+  return Math.min(leftCounter, rightCounter) + currentCounter;
+}
 
-function solution(name){
+function solution(name) {
   let possibleIndices = [];
-  for( let i = 0; i < name.length; i++ ){
+  for (let i = 0; i < name.length; i++) {
     possibleIndices.push(i);
   }
   possibleIndices = createPossibleIndices(name, possibleIndices);
   return solutionImplementation(name, 0, possibleIndices);
 }
 
-function calcNextIndex(name, currentIndex, possibleIndices, direction){
-  const distances = new Map();
+function calcNextIndex(name, currentIndex, possibleIndices, direction) {
+  // const distances = new Map();
+  let distance = 0;
+  let tempIndex = currentIndex;
   possibleIndices.forEach((targetIndex) => {
-    let distance = 0;
-    let tempIndex = currentIndex;
-    if( direction === "RIGHT" ){
+    if (direction === "RIGHT") {
       while (tempIndex !== targetIndex) {
         //오른쪽으로 움직이며 최소 거리를 계산하는 코드.
         distance++;
         tempIndex++;
         tempIndex = tempIndex % name.length;
       }
-    }else if( direction === "LEFT" ){
+    } else if (direction === "LEFT") {
       while (tempIndex !== targetIndex) {
         //왼쪽으로 움직이며 최소 거리를 계산하는 코드.
         distance++;
@@ -44,22 +71,21 @@ function calcNextIndex(name, currentIndex, possibleIndices, direction){
         if (tempIndex < 0) tempIndex = name.length + tempIndex;
       }
     }
-    distances.set(targetIndex, distance); // targetIndex에 도달하기 까지의 시간.
+    // distances.set(targetIndex, distance); // targetIndex에 도달하기 까지의 시간.
   });
-  distances.delete(currentIndex);
-  let min = ["", Number.POSITIVE_INFINITY];
-  distances.forEach((distance, key) => {
-    min[1] = min[1] < distance ? min[1] : distance;
-    min[0] = min[1] < distance ? min[0] : key;
-  });
-  return min[0];
+  return [tempIndex, distance];
+  // distances.delete(currentIndex);
+  // let min = ["", Number.POSITIVE_INFINITY];
+  // distances.forEach((distance, key) => {
+  //   min[1] = min[1] < distance ? min[1] : distance;
+  //   min[0] = min[1] < distance ? min[0] : key;
+  // });
 }
 
 function createPossibleIndices(name, possibleIndices) {
   let tempIndices = [];
   possibleIndices.forEach((targetIndex) => {
-    if (name[targetIndex] !== "A")
-      tempIndices.push(targetIndex);
+    if (name[targetIndex] !== "A") tempIndices.push(targetIndex);
   });
   return tempIndices;
 }
@@ -92,5 +118,10 @@ function matchCharacter(name, i) {
 }
 // matchCharacter("JEROEN", 1);
 
-console.log(solution("BG"));
+// console.log(solution("BG"));
 // console.log(solution("BDFADHA"));
+// console.log(solution("JEROEN"));
+console.log(solution("CBD"));
+// console.log(solution("AAAK"))
+
+// console.log( calcNextIndex("AAAK",0,[3],"LEFT" ) );
